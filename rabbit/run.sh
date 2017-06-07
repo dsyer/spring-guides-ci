49693ea6f5cd78f887a9c62919e4e60b262abe6b
Dave Syer <dsyer@pivotal.io>
2017-06-07 10:22:56 +0100

    Use vfs storage

--- rabbit/run.sh
+++ rabbit/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dockerd --storage-driver=btrfs --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
+dockerd --storage-driver=vfs --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
 
 docker-compose create
 docker-compose start
