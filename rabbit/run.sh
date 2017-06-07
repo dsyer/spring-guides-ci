2a0a2e14d14bac9e3814d12c87e89b2df7d92e5d
Dave Syer <dsyer@pivotal.io>
2017-06-07 10:35:38 +0100

    Use rabbit base image instead of docker

--- rabbit/run.sh
+++ rabbit/run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-dockerd --storage-driver=vfs --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
-
-docker-compose create
-docker-compose start
+service rabbitmq-server start
